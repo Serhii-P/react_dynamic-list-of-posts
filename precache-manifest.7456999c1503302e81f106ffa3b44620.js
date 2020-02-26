706d84aebde2fc00736f55bda0d22b185c2bbdd7
Serhii-P <serhii.pyskun@gmail.com>
2020-02-26 16:13:26 +0200

    Updates

---
+++ precache-manifest.7456999c1503302e81f106ffa3b44620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132260e11fa140557dcbe688466be9f8",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "73cda609370b280cb302",
+    "url": "/react_dynamic-list-of-posts/static/css/main.333710e4.chunk.css"
+  },
+  {
+    "revision": "278b66de031352346cb0",
+    "url": "/react_dynamic-list-of-posts/static/js/2.836ca02e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_dynamic-list-of-posts/static/js/2.836ca02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cda609370b280cb302",
+    "url": "/react_dynamic-list-of-posts/static/js/main.fa5251af.chunk.js"
+  },
+  {
+    "revision": "e57bd30df291ae2a77df",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime-main.4dee9116.js"
+  }
+]);
